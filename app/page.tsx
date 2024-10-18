d39385409b3995860a7ed37d4a8e34061782425d
JamesMatthew07 <llanosjamesmatthew30@gmail.com>
2024-10-19 03:02:20 +0800

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Page() {
         title: "Bernese Oberland",
         description: "The Bernese Oberland is a popular Swiss holiday region south-east of the city Bern. You’ll find a very diverse landscape here with lakes, high peaks, scenic mountain villages and much, much more.",
         link: "https://www.berneseoberlandpass.ch/maps-bernese-oberland/",
-        image: "/assets/bernese.jpg",
+        image: "/assets/bernese.JPG",
         width: 0,
         height: 0
       },{
@@ -42,14 +42,14 @@ export default function Page() {
         title: "Jungfrau",
         description: "Jungfrau, at 4,158 meters (13,642 feet) in the Swiss Bernese Alps, is famous for its stunning glaciers and breathtaking views. It attracts outdoor enthusiasts with activities like skiing and hiking, especially from the Jungfraujoch, the 'Top of Europe.'",
         link: "https://www.jungfrau.ch/en-gb/",
-        image: "/assets/ricardo-gomez-angel-Qgph3PrfOFY-unsplash-scaled.jpg",
+        image: "/assets/ricardo-gomez-angel-Qgph3PrfOFY-unsplash-scaled.JPG",
         width: 0,
         height: 0
       },
       {
         title: "Zermatt",
         description: "Experience breathtaking views of the iconic Matterhorn while exploring charming streets and indulging in outdoor adventures. Zermatt offers skiing, hiking, and an enchanting alpine atmosphere.",
-        link: "/assets/ricardo-gomez-angel-Qgph3PrfOFY-unsplash-scaled.jpg'",
+        link: "/assets/ricardo-gomez-angel-Qgph3PrfOFY-unsplash-scaled.JPG'",
         image: "/assets/zermatt-matterhorn-switzerland-shutterstock_1298208013_44fea015e5.jpeg",
         width: 0,
         height: 0
@@ -57,16 +57,16 @@ export default function Page() {
       {
         title: "Interlaken",
         description: "Nestled between Lake Thun and Lake Brienz, Interlaken is a hub for adventure sports and offers stunning views of the surrounding mountains. Enjoy activities like paragliding, hiking, and boat trips in this picturesque destination.",
-        link: "/assets/Interlaken.jpg",
-        image: "/assets/ricardo-gomez-angel-Qgph3PrfOFY-unsplash-scaled.jpg",
+        link: "/assets/Interlaken.JPG",
+        image: "/assets/ricardo-gomez-angel-Qgph3PrfOFY-unsplash-scaled.JPG",
         width: 0,
         height: 0
       },
       {
         title: "Grindelwald",
         description: "Discover the beauty of Grindelwald, where dramatic mountain scenery meets cozy Swiss chalets. This destination is perfect for hiking, skiing, and enjoying the breathtaking views of the Eiger North Face.",
-        link: "/assets/ricardo-gomez-angel-Qgph3PrfOFY-unsplash-scaled.jpg'",
-        image: "/assets/Grindelwald.jpg",
+        link: "/assets/ricardo-gomez-angel-Qgph3PrfOFY-unsplash-scaled.JPG'",
+        image: "/assets/Grindelwald.JPG",
         width: 0,
         height: 0
       },
